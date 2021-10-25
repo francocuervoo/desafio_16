4f0d3498b27eaca80b994ecb9e09a889a22950bd
Franco <franco@perfit.com.ar>
2021-10-25 13:38:46 -0300

    Desafio14

--- src/utils/random.numbers.util.js
+++ src/utils/random.numbers.util.js
@@ -1,9 +1,25 @@
-let listaRepetidos = {};
-for (let i = 1; i < cantidad; i++) {
-  let numeroRandom = Math.floor(Math.random() * 1000) + 1;
-  if (!listaRepetidos[numeroRandom]) {
-    listaRepetidos[numeroRandom] = 0;
+export const listaRepetidosData = (numero) => {
+  let listaRepetidos = {};
+  for (let i = 1; i < numero; i++) {
+    let numeroRandom = Math.floor(Math.random() * 1000) + 1;
+    if (!listaRepetidos[numeroRandom]) {
+      listaRepetidos[numeroRandom] = 0;
+    }
+    listaRepetidos[numeroRandom]++;
   }
-  listaRepetidos[numeroRandom]++;
+  return listaRepetidos;
+};
+
+process.send("ready");
+
+process.on("message", (numero) => {
+  const resultado = listaRepetidosData(numero);
+  process.send(resultado);
+});
+
+if (respuestaChild == "ready") {
+  randomsFork.send(numero);
+} else {
+  const resultadoJson = JSON.stringify(respuestaChild);
+  res.status(200).send(resultadoJson);
 }
-return listaRepetidos;
